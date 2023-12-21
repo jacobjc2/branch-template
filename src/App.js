20f2d7b8404b19a783b9fba07a0e3a327a676a6f
Jacob Christensen Admin <jChristensenAdmin@weimerbearing.com>
2023-12-21 15:23:13 -0600

    Added initial routing and navigation to top level pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { Navigation } from './components/Navigation';
 import { Footer } from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
+import { AboutPage } from './pages/about/AboutPage';
+import { ContactPage } from './pages/contact/ContactPage';
+import { IndustriesPage } from './pages/industries/IndustriesPage';
+import { ManufacturersPage } from './pages/manufacturers/ManufacturersPage';
+import { ProductsPage } from './pages/products/ProductsPage';
+import { ServicesPage } from './pages/services/ServicesPage';
 
 function App() {
   return (
@@ -11,7 +17,12 @@ function App() {
         <Navigation />
           <Routes>
             <Route index element={<HomePage/>}/>
-            {/* <Route path='contact' element={<ContactPage/>}/> */}
+            <Route path='products' element={<ProductsPage/>}/>
+            <Route path='services' element={<ServicesPage/>}/>
+            <Route path='industries' element={<IndustriesPage/>}/>
+            <Route path='manufacturers' element={<ManufacturersPage/>}/>
+            <Route path='contact' element={<ContactPage/>}/>
+            <Route path='about' element={<AboutPage/>}/>
           </Routes>
           <Footer/>
         </Router>
